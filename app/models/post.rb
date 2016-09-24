840db2d9465cd96aa61f31c989a4aadb9bca4204
Carol Yao <yaocarol96@gmail.com>
2016-09-23 22:48:48 -0400

    added method to specify length of time since post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+
+    def hours_ago
+      @time = ((Time.now - created_at).to_i) / 60
+      if @time < 60
+        "#{@time} min ago"
+      elsif (@time >= 60) && (@time < 1440 )
+        "#{@time / 60} hours ago"
+      else
+        "#{@time / 1449} days ago"
+      end
+    end
 end
