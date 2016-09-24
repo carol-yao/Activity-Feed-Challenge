107e7bb525a9a7ee195f07a644000cbe72f769ec
Carol Yao <yaocarol96@gmail.com>
2016-09-23 22:24:42 -0400

    arranged posts by descending order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def feed
     @user = User.first
     @post = @user.posts.build
-    @ownposts = User.first.posts
-    @friendposts = User.first.friend_posts
+    @ownposts = User.first.posts.order('posts.created_at DESC')
+    @friendposts = User.first.friend_posts.order('friend_posts.created_at DESC')
     @allposts = [@ownposts, @friendposts].flatten #group all posts together
   end
 end
